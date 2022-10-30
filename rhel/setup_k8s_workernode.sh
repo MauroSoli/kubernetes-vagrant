2275472c245c164fa7860a7390f560192eb4c891
MauroSoli <74069420+MauroSoli@users.noreply.github.com>
2022-10-30 19:15:43 +0100

    add yaml file to worker node to set nodeip

--- rhel/setup_k8s_workernode.sh
+++ rhel/setup_k8s_workernode.sh
@@ -23,9 +23,29 @@ done
 # Calculation ca hash need for --discovery-token-ca-cert-hash
 CaHASH="$(openssl x509 -in /vagrant/certificates/ca.crt -noout -pubkey | \
 openssl rsa -pubin -outform DER 2>/dev/null | sha256sum | cut -d' ' -f1)"
+
+#Kubeadm config file generation
+cat << EOF > /tmp/kubeadm-worker-config.yaml
+apiVersion: kubeadm.k8s.io/v1beta3
+kind: JoinConfiguration
+discovery:
+  bootstrapToken:
+    apiServerEndpoint: kubebalancer01:6443
+    token: "$TOKEN"
+    caCertHashes:
+    - sha256:$CaHASH
+nodeRegistration:
+  kubeletExtraArgs:
+    node-ip: "$IP_ADDR"
+---
+apiVersion: kubeadm.k8s.io/v1beta3
+kind: ClusterConfiguration
+kubernetesVersion: stable
+controlPlaneEndpoint: kubebalancer01
+networking:
+  podSubnet: "10.244.0.0/16"
+EOF
+
 # Join to K8S Cluster as a workernode
 kubeadm join kubebalancer01:6443 \
-      --token $TOKEN \
-      --discovery-token-ca-cert-hash sha256:$CaHASH \
-      --apiserver-advertise-address:$IP_ADDR
-
+      --config /tmp/kubeadm-worker-config.yaml
